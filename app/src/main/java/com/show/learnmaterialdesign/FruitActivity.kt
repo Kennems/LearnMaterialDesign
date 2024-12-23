2825919b8dd67b603f15226373e5055becd85130
kennems <kennem@foxmail.com>
2024-12-23 23:27:32 +0800

    20241223

--- app/src/main/java/com/show/learnmaterialdesign/FruitActivity.kt
+++ app/src/main/java/com/show/learnmaterialdesign/FruitActivity.kt
@@ -1,20 +1,53 @@
 package com.show.learnmaterialdesign
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.show.learnmaterialdesign.databinding.ActivityFruitBinding
 
 class FruitActivity : AppCompatActivity() {
+    private val mBinding
+            by lazy { ActivityFruitBinding.inflate(layoutInflater) }
+
+    companion object {
+        const val FRUIT_NAME = "fruit_name"
+        const val FRUIT_IMAGE_ID = "fruit_image_id"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_fruit)
+        setContentView(mBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val fruitName = intent.getStringExtra(FRUIT_NAME) ?: ""
+        val fruitImageId = intent.getIntExtra(FRUIT_IMAGE_ID, 0)
+        mBinding.apply {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            collapsingToolbar.title = fruitName
+            Glide.with(this@FruitActivity).load(fruitImageId).into(fruitImageView)
+            fruitContentText.text = generateFruitContent(fruitName)
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun generateFruitContent(fruitName: String) = fruitName.repeat(500)
 }
--- app/src/main/java/com/show/learnmaterialdesign/adapter/FruitAdapter.kt
+++ app/src/main/java/com/show/learnmaterialdesign/adapter/FruitAdapter.kt
@@ -1,6 +1,7 @@
 package com.show.learnmaterialdesign.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.show.learnmaterialdesign.FruitActivity
 import com.show.learnmaterialdesign.R
 import com.show.learnmaterialdesign.entity.Fruit
 
@@ -20,9 +22,18 @@ class FruitAdapter(val context: Context, val fruitList: List<Fruit>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.fruit_item, parent, false)
-        return ViewHolder(view)
+        val holder = ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            val fruit = fruitList[position]
+            val intent = Intent(context, FruitActivity::class.java).apply {
+                putExtra(FruitActivity.FRUIT_NAME, fruit.name)
+                putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.imageId)
+            }
+            context.startActivity(intent)
+        }
+        return holder
     }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val fruit = fruitList[position]
         holder.fruitName.text = fruit.name
